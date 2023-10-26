84bfd398d6b181f1feeb6fa8b855eb1597f8006d
YayakFirmansyah <yakizam123@gmail.com>
2023-10-26 23:14:41 +0700

    praktikum 2.7

--- lib/main.dart
+++ lib/main.dart
@@ -12,10 +12,10 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blueAccent),
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.red),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Tugas Pertama Flutter'),
+      home: const MyHomePage(title: 'MyApp'),
     );
   }
 }
@@ -29,26 +29,134 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTabController(
+      length: 2,
+      child: Scaffold(
+          appBar: AppBar(
+            bottom: const TabBar(tabs: [
+              Tab(
+                text: 'BERITA TERBARU',
+              ),
+              Tab(
+                text: 'PERTANDINGAN HARI INI',
+              )
+            ]),
+            backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+            title: Text(widget.title),
+          ),
+          body: const TabBarView(
+            children: [FirstTab(), FirstTab()],
+          )),
+    );
+  }
+}
+
+class FirstTab extends StatelessWidget {
+  const FirstTab({
+    super.key,
+  });
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
-      body: const Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              "Dhiya'uddin Firmansyah",
-            ),
-            Text(
-              "2141720247",
-            ),
-          ],
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.start,
+      children: <Widget>[
+        Container(
+          decoration:
+              BoxDecoration(border: Border.all(color: Colors.purpleAccent)),
+          margin: EdgeInsets.all(10.0),
+          child: Column(
+            children: [
+              Image.asset("assets/images/header.jpeg"),
+              Container(
+                color: Colors.white,
+                padding: EdgeInsets.all(8.0),
+                child: Text("Ini adalah gawang"),
+              ),
+              Container(
+                width: double.infinity,
+                color: Colors.purpleAccent,
+                padding: EdgeInsets.all(8.0),
+                child: Text("Property"),
+              ),
+            ],
+          ),
         ),
+        Expanded(
+          child: ListView(
+            children: [
+              SecondWidget(),
+              SecondWidget(),
+              SecondWidget(),
+              SecondWidget(),
+            ],
+          ),
+        )
+      ],
+    );
+  }
+}
+
+class SecondWidget extends StatelessWidget {
+  const SecondWidget({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(border: Border.all(color: Colors.blueAccent)),
+      margin: EdgeInsets.all(10.0),
+      child: Column(
+        children: [
+          Row(
+            children: [
+              Expanded(
+                child: Image.asset("assets/images/header.jpeg"),
+              ),
+              Expanded(
+                child: Container(
+                  color: Colors.white,
+                  padding: EdgeInsets.all(8.0),
+                  child: Text("Ini adalah gawang pada saat malam hari"),
+                ),
+              ),
+            ],
+          ),
+          const Divider(
+            height: 0,
+            color: Colors.blueAccent,
+          ),
+          Container(
+            width: double.infinity,
+            padding: EdgeInsets.all(8.0),
+            child: Text("Barcelona Feb 13, 2021"),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class NewWidget extends StatelessWidget {
+  final String text;
+  final Color color;
+  const NewWidget({
+    super.key,
+    required this.text,
+    required this.color,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Container(
+        color: color,
+        height: 50.0,
+        alignment: Alignment.center,
+        child: Text(text),
       ),
     );
   }
